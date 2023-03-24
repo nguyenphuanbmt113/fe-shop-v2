1ff4374d1cfa280a1a5edc7e18de1b0f8af488fb
nguyenphuanbmt113 <nguyenphuanbmt113@gmail.com>
2023-03-24 15:57:54 +0700

    fix10

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
 import "./index.scss";
-import "./App.scss";
 import { store } from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
